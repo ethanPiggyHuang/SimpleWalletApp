17a8d8d2897508fb09956fdc09daed32a7bd902d
ethanhuang <makoto31996@gmail.com>
2023-06-14 23:07:02 +0800

    feat(feature2): print transaction info with 2 transaction hashs using promise.allSettled

--- src/featrue/Transactions.tsx
+++ src/featrue/Transactions.tsx
@@ -3,24 +3,33 @@ import styled from 'styled-components/macro';
 import Web3, { TransactionInfo } from 'web3';
 
 export const Transactions: React.FC = () => {
-  const title = 'Account Info';
-  const transactionHashs =
-    '0x1eb6aab282d701d3d2eeb762bd426df625767e68ebf9c00b484905be1343304e';
-  const [transactionData, setTransactionData] =
-    useState<TransactionInfo | null>(null);
+  const title = 'Transactions';
+  const transactionHashs = [
+    '0x1eb6aab282d701d3d2eeb762bd426df625767e68ebf9c00b484905be1343304e',
+    '0xf134054861dccf1f211e6fd92808475b2fb290489a4e41bc008260d8cc58b9f9',
+  ];
   const web3 = new Web3(
     `https://mainnet.infura.io/v3/${process.env.REACT_APP_INFURA_APIKEY}`
   );
+  const [data, setData] = useState<{ [key: string]: TransactionInfo }>({});
 
   useEffect(() => {
     async function getData() {
-      const response = await web3.eth.getTransaction(transactionHashs);
+      const promise1 = web3.eth.getTransaction(transactionHashs[0]);
+      const promise2 = web3.eth.getTransaction(transactionHashs[1]);
 
-      console.log('response', response);
-      console.log('blockNumber', response.blockNumber);
-      console.log(response.blockNumber);
-      let obj = response;
-      setTransactionData(obj);
+      Promise.allSettled([promise1, promise2]).then((results) => {
+        results.forEach((result, index) => {
+          console.log(index, result.status);
+          if (result.status === 'fulfilled') {
+            const object = {} as { [index: string]: TransactionInfo };
+            object[`${index}`] = result.value;
+            setData((prev) => {
+              return { ...prev, ...object };
+            });
+          }
+        });
+      });
     }
     getData();
   }, []);
@@ -28,24 +37,30 @@ export const Transactions: React.FC = () => {
   return (
     <Wrapper>
       <SectionTitle>{title}</SectionTitle>
-      <div>
-        <InfoLine>
-          <span>Transaction</span>
-          <span>{transactionHashs}</span>
-        </InfoLine>
-        <InfoLine>
-          <span>BlockNumber</span>
-          <span>{transactionData && `${transactionData?.blockNumber}`}</span>
-        </InfoLine>
-        <InfoLine>
-          <span>From</span>
-          <span>{transactionData && `${transactionData?.from}`}</span>
-        </InfoLine>
-        <InfoLine>
-          <span>To</span>
-          <span>{transactionData && `${transactionData?.to}`}</span>
-        </InfoLine>
-      </div>
+      {transactionHashs.map((transactionHash, index) => (
+        <div key={transactionHash}>
+          <InfoLine>
+            <span>TX Hash</span>
+            <span>{transactionHash}</span>
+          </InfoLine>
+          <InfoLine>
+            <span>Block</span>
+            <span>
+              {data[index.toString()]?.blockNumber
+                ? Number(data[index.toString()]?.blockNumber)
+                : ''}
+            </span>
+          </InfoLine>
+          <InfoLine>
+            <span>From</span>
+            <span>{data[index.toString()]?.from}</span>
+          </InfoLine>
+          <InfoLine>
+            <span>To</span>
+            <span>{data[index.toString()]?.to}</span>
+          </InfoLine>
+        </div>
+      ))}
     </Wrapper>
   );
 };
