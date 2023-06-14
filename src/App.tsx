2ee2534202063839a5b1688948b5feef67b9d5fc
ethanhuang <makoto31996@gmail.com>
2023-06-14 20:27:48 +0800

    feat(feature1): print eth balance with right digit/unit given account address, without style.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { AccountInfo } from './featrue/AccountInfo';
+import { createGlobalStyle } from 'styled-components';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          simple-wallet-app
-        </a>
-      </header>
+      <GlobalStyle />
+      <AccountInfo />
     </div>
   );
 }
 
 export default App;
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Noto Sans', sans-serif;
+  }
+  
+  #root {
+    min-height: 100vh;
+    position: relative;
+    @media screen and (max-width: 991px) {
+    }
+  }
+`;
---
+++ src/featrue/AccountInfo.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components/macro';
+import Web3 from 'web3';
+
+export const AccountInfo: React.FC = () => {
+  const title = 'Account Info';
+  const address = '0x33b8287511ac7F003902e83D642Be4603afCd876';
+  const [ethBalance, setEthBalance] = useState('');
+  const web3 = new Web3(
+    `https://mainnet.infura.io/v3/${process.env.REACT_APP_INFURA_APIKEY}`
+  );
+
+  useEffect(() => {
+    async function getBalance() {
+      web3.eth.getBalance(address).then((response) => {
+        const balanceInEther = web3.utils.fromWei(response, 'ether');
+        setEthBalance(balanceInEther);
+      });
+    }
+    getBalance();
+  }, []);
+
+  return (
+    <Wrapper>
+      <SectionTitle>{title}</SectionTitle>
+      <div>
+        <InfoLine>
+          <span>Account Address</span>
+          <span>{address}</span>
+        </InfoLine>
+        <InfoLine>
+          <span>ETH Balance</span>
+          <span>{`${ethBalance} ETH`}</span>
+        </InfoLine>
+      </div>
+    </Wrapper>
+  );
+};
+
+type WrapperProps = {};
+
+const Wrapper = styled.div<WrapperProps>`
+  margin: auto;
+  width: 774px;
+  border: 1px solid black;
+`;
+
+const SectionTitle = styled.h2`
+  font-size: 20px;
+  text-align: left;
+`;
+
+const InfoLine = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
