64432dc9dde76c8e38a506267e053c57e98ecfa8
ethanhuang <makoto31996@gmail.com>
2023-06-19 15:13:34 +0800

    refactor(main/.env): move value of address and transaction hashs into .env file.

--- src/featrue/AccountInfo.tsx
+++ src/featrue/AccountInfo.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import web3Api from '../utils/web3';
 import { shortenText } from '../utils/common';
 import { Template } from '../components/Template';
